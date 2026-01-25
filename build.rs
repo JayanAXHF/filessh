96537fe5a364430a290984b8b2f7841838180a14
JayanAXHF <sunil.chdry@gmail.com>
2026-01-25 17:12:05 +0530

    perf: streamlined build.rs

--- build.rs
+++ build.rs
@@ -8,15 +8,6 @@ use vergen_gix::{BuildBuilder, CargoBuilder, Emitter, GixBuilder};
 mod cli;
 
 fn main() -> Result<()> {
-    let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
-
-    let cmd = cli::Cli::command().name("filessh");
-    let man = clap_mangen::Man::new(cmd);
-    let mut buffer: Vec<u8> = Default::default();
-    man.render(&mut buffer)?;
-
-    std::fs::write(out_dir.join("filessh.1"), buffer)?;
-
     let build = BuildBuilder::all_build()?;
     let gix = GixBuilder::all_git()?;
     let cargo = CargoBuilder::all_cargo()?;
